49cd1c6c1131da8fab085b49516f1295d4fc081c
Amanda <jesslynmulyawan@gmail.com>
2025-04-30 10:14:06 +0700

    Initial Commit

---
+++ CoBo-iPad/CoBo_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoBo_iPadApp.swift
+//  CoBo-iPad
+//
+//  Created by Amanda on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CoBo_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoBo-iPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoBo-iPad
+//
+//  Created by Amanda on 30/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
